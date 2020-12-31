3fdd254b9c6ad756fecda1e744244470a1700477
Yuchi Su <astraiostor@gmail.com>
2020-12-31 19:05:08 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-// 載入 mongoose
-const mongoose = require('mongoose')
 // 載入 handlebars
 const exphbs = require('express-handlebars')
 // 引用 body-parser
@@ -11,24 +9,10 @@ const methodOverride = require('method-override')
 
 // 引用路由器
 const routes = require('./routes')
+// 引用 mongoose
+require('./config/mongoose')
 
 const app = express()
-
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected')
-})
 
 // setting template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
