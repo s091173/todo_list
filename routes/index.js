a2bbf8903a2ba58b53f46cecc2dd6715d2cc462b
Yuchi Su <astraiostor@gmail.com>
2021-03-08 22:48:25 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ const router = express.Router()
 // 引入 home 模組程式碼
 const home = require('./modules/home')
 const todos = require('./modules/todos')
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
