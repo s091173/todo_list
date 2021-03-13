ab8322c79e0ab9a9582ad9fc16d7dd9bea4bf398
Yuchi Su <astraiostor@gmail.com>
2021-03-13 14:13:49 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,15 @@ const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
 
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
+// 掛載 auth.js middleware
+const { authenticator } = require('../middleware/auth')
+
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 // 將網址結構符合 /users 字串開頭的 request 導向 users 模組 
 router.use('/users', users)
+// 將網址結構符合 / 字串的 request 導向 home 模組
+router.use('/', authenticator, home)
 
 // 匯出路由器
 module.exports = router
